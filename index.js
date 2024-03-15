38b74e1dd58aef59cc72f454b54ead63837517c5
Matheus-Souto <emaildopaypal007@gmail.com>
2024-03-15 02:45:23 -0300

    config supabase

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
